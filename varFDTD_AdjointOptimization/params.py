593c62d5d9cebd8c99e3c4cae61c02baf6de4b4b
elbarbarosso510 <el_barbarosso510@protonmail.com>
2024-05-15 16:40:42 +0300

    Fix bug in login

---
+++ varFDTD_AdjointOptimization/params.py
@@ -0,0 +1,9 @@
+
+dev_params = {'wg01': 0.5e-6,
+              'wg02': 0.5e-6,
+              'spacing': 0.35e-6,
+              'length':6.0e-6,
+              'width': 6.0e-6,      # Target Width
+              'in_offset': 0.0e-6,
+              'polarization': "E mode (TE)",
+              'num_points': 21}
